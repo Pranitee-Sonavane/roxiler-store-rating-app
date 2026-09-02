cb20406f46f4ec6b4cc8f4e0242446fc43de2b63
Pranitee Sonavane <sonavanepranitee@gmail.com>
2026-09-02 23:07:26 +0530

    PostgreSQL connection done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const cors = require("cors");
 const express = require("express");
-
+const pool = require("./config/db");
 const app = express();
-app.use(cors());
 
+console.log("Pool:", pool);
+console.log("Pool query:", typeof pool.query);
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-res.send("Roxiler Store Rating API is running!!");});
+    res.send("Roxiler Store Rating API is running!");
+});
 
-app.listen(5000, () => {
+app.listen(5000, async () => {
     console.log("Server running on port 5000");
+
+    try {
+        await pool.query("SELECT NOW()");
+        console.log("PostgreSQL connected successfully!");
+    } catch (error) {
+        console.error("Database connection failed:", error.message);
+    }
 });
